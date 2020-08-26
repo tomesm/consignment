35f86d3ff4e39b1e20f76810ad0bbc2488f2ddd0
Martin Tomes <tomesm@gmail.com>
2020-08-26 09:20:58 +0200

    Added consignment client and service

---
+++ consignment-service/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+
+	// Import the generated protobuf code
+	"fmt"
+
+	micro "github.com/micro/go-micro"
+	pb "github.com/tomesm/consignment/consignment-service/proto/consignment"
+	"golang.org/x/net/context"
+)
+
+type IRepository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.consignments = append(repo.consignments, consignment)
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo IRepository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	res.Consignments = s.repo.GetAll()
+	return nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+	srv.Init()
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
